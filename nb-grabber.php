90733facfc65b84cc7f1dc9094e2ec7fdfac29ec
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2014-03-11 20:46:08 -0700

    added writer targetting to runner script.

--- nb-grabber.php
+++ nb-grabber.php
@@ -1,11 +1,29 @@
 <?php
 
-if (2 !== $argc) {
-    die("Usage: php nb-grabber <agency-name>\n");
+$usage = "Usage: php nb-grabber <agency-name>\n [xml|csv-schedule]";
+
+if (3 !== $argc) {
+    die($usage);
 }
+
+if (! in_array($argv[2], array('xml', 'csv-schedule'))) {
+    die($usage);
+}
+
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$writer = new nbgrabber\XmlWriter(__DIR__);
+// @todo use factory instead
+switch ($argv[2]) {
+    case 'xml' :
+        $writer = new nbgrabber\XmlWriter(__DIR__);
+        break;
+    case 'csv-schedule' :
+        $writer = new nbgrabber\CsvSchedulePrinter(__DIR__);
+        break;
+    default:
+        die($usage);
+}
+
 $grabber = new nbgrabber\Grabber($writer, $argv[1]);
 $grabber->export();
